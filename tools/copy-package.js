9a97e0a1413825b02d7de2e8c5c59ad57d2c941a
lujo5 <lukamatosevic5@gmail.com>
2017-10-07 13:57:15 +0200

    Added task to copy readme and license to dist.

--- tools/copy-package.js
+++ tools/copy-package.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
+
+// Copy package.json to dist
 let resizable = fs.readFileSync('package.json').toString();
 fs.writeFileSync('dist/package.json', resizable);
+
+// Copy README.md to dist
+resizable = fs.readFileSync('README.md').toString();
+fs.writeFileSync('dist/README.md', resizable);
+
+// Copy LICENSE to dist
+resizable = fs.readFileSync('LICENSE').toString();
+fs.writeFileSync('dist/LICENSE', resizable);
